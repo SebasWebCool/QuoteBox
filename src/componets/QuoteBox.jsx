483703d2bdb25d1de89949757280e58de1b970b3
Sebas <sebasweb2000@gmail.com>
2022-08-28 13:45:07 -0400

    responsive

--- src/componets/QuoteBox.jsx
+++ src/componets/QuoteBox.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function QuoteBox({Frase,Color,GetAll}) {
+function QuoteBox({ Frase, Color, GetAll }) {
 
-  let objColor={
-    color:Color
+  let objColor = {
+    color: Color
   }
   let objBackground = {
     backgroundColor: Color
@@ -11,14 +11,16 @@ function QuoteBox({Frase,Color,GetAll}) {
 
   return (
     <div className='all' style={objBackground}>
-        <div className='cont_fr' style={objColor}>
-            <div className='cont_i-frase'>
-              <i className='bx bxs-quote-left'></i>
-              <p className='frase'>{Frase.quote}</p>
-            </div>
-            <p className='author'>{Frase.author}</p>
-            <button className='btn'style={objBackground} onClick={GetAll}>&gt;</button>
+      <div className='cont_fr' style={objColor}>
+        <div className='cont_i-frase'>
+          <i className='bx bxs-quote-left'></i>
+          <p className='frase'>{Frase.quote}</p>
         </div>
+        <div className="author_tbn">
+          <p className='author'>{Frase.author}</p>
+          <button className='btn' style={objBackground} onClick={GetAll}>&gt;</button>
+        </div>
+      </div>
     </div>
   )
 }
